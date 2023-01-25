16f918787e22d32d228c2623c5f665847266e714
forks <redforks@gmail.com>
2023-01-25 23:12:33 +0800

    Add swagger-ui

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,12 @@
 use std::net::{Ipv4Addr, SocketAddr};
-use warp::Filter;
+use std::sync::Arc;
+use warp::{Filter, Rejection, Reply};
+use warp::http::{Response, StatusCode, Uri};
+use warp::path::{FullPath, Tail};
 use utoipa::{
     OpenApi
 };
+use utoipa_swagger_ui::Config;
 
 #[derive(OpenApi)]
 #[openapi(
@@ -12,13 +16,21 @@ struct ApiDoc;
 
 #[tokio::main]
 async fn main() {
+    let config = Arc::new(Config::from("/api-doc.json"));
+    let swagger_ui = warp::path("swagger-ui")
+        .and(warp::get())
+        .and(warp::path::full())
+        .and(warp::path::tail())
+        .and(warp::any().map(move || config.clone()))
+        .and_then(serve_swagger);
+
     let api_doc = warp::path("api-doc.json")
         .and(warp::get())
         .map(|| warp::reply::json(&ApiDoc::openapi()));
-    let home = warp::path("home")
+    let home = warp::path("hello")
         .and(warp::get())
         .map(hello);
-    let web = api_doc.or(home);
+    let web = api_doc.or(home).or(swagger_ui);
 
     let addr = SocketAddr::from((Ipv4Addr::UNSPECIFIED, 3030));
     warp::serve(web).run(addr).await
@@ -33,4 +45,36 @@ fn hello() -> impl warp::Reply {
     warp::reply::html(
         "<h1>Hello, world!</h1>".to_string(),
     )
+}
+
+async fn serve_swagger(
+    full_path: FullPath,
+    tail: Tail,
+    config: Arc<Config<'static>>,
+) -> Result<Box<dyn Reply + 'static>, Rejection> {
+    if full_path.as_str() == "/swagger-ui" {
+        return Ok(Box::new(warp::redirect::found(Uri::from_static(
+            "/swagger-ui/",
+        ))));
+    }
+
+    let path = tail.as_str();
+    match utoipa_swagger_ui::serve(path, config) {
+        Ok(file) => {
+            if let Some(file) = file {
+                Ok(Box::new(
+                    Response::builder()
+                        .header("Content-Type", file.content_type)
+                        .body(file.bytes),
+                ))
+            } else {
+                Ok(Box::new(StatusCode::NOT_FOUND))
+            }
+        }
+        Err(error) => Ok(Box::new(
+            Response::builder()
+                .status(StatusCode::INTERNAL_SERVER_ERROR)
+                .body(error.to_string()),
+        )),
+    }
 }
