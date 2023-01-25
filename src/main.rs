2f7c85db22f366d12426816a5e031b95e0d19457
forks <redforks@gmail.com>
2023-01-25 23:05:29 +0800

    Add api-doc.json handler

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,36 @@
 use std::net::{Ipv4Addr, SocketAddr};
 use warp::Filter;
+use utoipa::{
+    OpenApi
+};
+
+#[derive(OpenApi)]
+#[openapi(
+paths(hello)
+)]
+struct ApiDoc;
 
 #[tokio::main]
 async fn main() {
-    let web = warp::path("home")
+    let api_doc = warp::path("api-doc.json")
         .and(warp::get())
-        .then(|| async {
-        warp::reply::html(
-            "<h1>Hello, world!</h1>".to_string(),
-        )
-    });
+        .map(|| warp::reply::json(&ApiDoc::openapi()));
+    let home = warp::path("home")
+        .and(warp::get())
+        .map(hello);
+    let web = api_doc.or(home);
 
     let addr = SocketAddr::from((Ipv4Addr::UNSPECIFIED, 3030));
     warp::serve(web).run(addr).await
+}
+
+#[utoipa::path(
+get,
+path = "/hello",
+responses((status = 200, description = "hello"))
+)]
+fn hello() -> impl warp::Reply {
+    warp::reply::html(
+        "<h1>Hello, world!</h1>".to_string(),
+    )
 }
