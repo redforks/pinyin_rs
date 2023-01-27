5da919c6348ae1d6a35f113880ef2dc39c950215
forks <redforks@gmail.com>
2023-01-27 10:18:38 +0800

    Parse FinalAndTons from str

--- pinyin/src/pinyin.rs
+++ pinyin/src/pinyin.rs
@@ -1,5 +1,7 @@
 use modular_bitfield::prelude::*;
-use std::fmt::{Display, Formatter};
+use std::error::Error;
+use std::fmt::{Debug, Display, Formatter};
+use std::str::FromStr;
 
 #[derive(
     Debug,
@@ -42,12 +44,22 @@ pub enum Initials {
 }
 
 #[derive(
-    Clone, Copy, PartialEq, Eq, BitfieldSpecifier, strum_macros::Display, strum_macros::AsRefStr,
+    Debug,
+    Clone,
+    Copy,
+    PartialEq,
+    Eq,
+    BitfieldSpecifier,
+    strum_macros::Display,
+    strum_macros::EnumIter,
+    strum_macros::AsRefStr,
+    strum_macros::IntoStaticStr,
 )]
 #[bits = 8]
 #[repr(u8)]
 #[strum(serialize_all = "snake_case")]
 pub enum Finals {
+    None,
     A,
     O,
     E,
@@ -73,10 +85,6 @@ pub enum Finals {
     IU,
     UA,
     UO,
-    Uai,
-    Uan,
-    Uang,
-    Ueng,
     #[strum(serialize = "ü")]
     V,
     #[strum(serialize = "üe")]
@@ -85,7 +93,16 @@ pub enum Finals {
     Van,
 }
 
-#[derive(Copy, Clone, PartialEq, Eq, BitfieldSpecifier, strum_macros::Display)]
+#[derive(
+    Debug,
+    Copy,
+    Clone,
+    PartialEq,
+    Eq,
+    BitfieldSpecifier,
+    strum_macros::Display,
+    strum_macros::EnumIter,
+)]
 #[bits = 3]
 pub enum Tones {
     #[strum(serialize = "")]
@@ -130,12 +147,179 @@ pub enum PinyinDisplay {
     FirstLetter(Pinyin),
 }
 
+#[derive(Clone, Copy, Debug, Eq, PartialEq)]
 struct FinalWithTones(Finals, Tones);
+
+#[derive(Debug)]
+struct FinalWithTonesFromStrError(String);
+
+impl Display for FinalWithTonesFromStrError {
+    fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
+        write!(f, "invalid final with tones: '{}'", self.0)
+    }
+}
+
+impl Error for FinalWithTonesFromStrError {}
+
+impl FromStr for FinalWithTones {
+    type Err = FinalWithTonesFromStrError;
+
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        let r = match s {
+            "a" => FinalWithTones(Finals::A, Tones::None),
+            "o" => FinalWithTones(Finals::O, Tones::None),
+            "e" => FinalWithTones(Finals::E, Tones::None),
+            "u" => FinalWithTones(Finals::U, Tones::None),
+            "i" => FinalWithTones(Finals::I, Tones::None),
+            "ai" => FinalWithTones(Finals::AI, Tones::None),
+            "ei" => FinalWithTones(Finals::EI, Tones::None),
+            "ou" => FinalWithTones(Finals::OU, Tones::None),
+            "an" => FinalWithTones(Finals::AN, Tones::None),
+            "en" => FinalWithTones(Finals::EN, Tones::None),
+            "ang" => FinalWithTones(Finals::Ang, Tones::None),
+            "eng" => FinalWithTones(Finals::Eng, Tones::None),
+            "er" => FinalWithTones(Finals::ER, Tones::None),
+            "ia" => FinalWithTones(Finals::IA, Tones::None),
+            "iā" => FinalWithTones(Finals::IA, Tones::One),
+            "iá" => FinalWithTones(Finals::IA, Tones::Two),
+            "iǎ" => FinalWithTones(Finals::IA, Tones::Three),
+            "ià" => FinalWithTones(Finals::IA, Tones::Four),
+            "ie" => FinalWithTones(Finals::IE, Tones::None),
+            "iē" => FinalWithTones(Finals::IE, Tones::One),
+            "ié" => FinalWithTones(Finals::IE, Tones::Two),
+            "iě" => FinalWithTones(Finals::IE, Tones::Three),
+            "iè" => FinalWithTones(Finals::IE, Tones::Four),
+            "iao" => FinalWithTones(Finals::Iao, Tones::None),
+            "iāo" => FinalWithTones(Finals::Iao, Tones::One),
+            "iáo" => FinalWithTones(Finals::Iao, Tones::Two),
+            "iǎo" => FinalWithTones(Finals::Iao, Tones::Three),
+            "iào" => FinalWithTones(Finals::Iao, Tones::Four),
+            "ian" => FinalWithTones(Finals::Ian, Tones::None),
+            "iān" => FinalWithTones(Finals::Ian, Tones::One),
+            "ián" => FinalWithTones(Finals::Ian, Tones::Two),
+            "iǎn" => FinalWithTones(Finals::Ian, Tones::Three),
+            "iàn" => FinalWithTones(Finals::Ian, Tones::Four),
+            "ing" => FinalWithTones(Finals::Ing, Tones::None),
+            "īng" => FinalWithTones(Finals::Ing, Tones::One),
+            "íng" => FinalWithTones(Finals::Ing, Tones::Two),
+            "ǐng" => FinalWithTones(Finals::Ing, Tones::Three),
+            "ìng" => FinalWithTones(Finals::Ing, Tones::Four),
+            "iang" => FinalWithTones(Finals::Iang, Tones::None),
+            "iāng" => FinalWithTones(Finals::Iang, Tones::One),
+            "iáng" => FinalWithTones(Finals::Iang, Tones::Two),
+            "iǎng" => FinalWithTones(Finals::Iang, Tones::Three),
+            "iàng" => FinalWithTones(Finals::Iang, Tones::Four),
+            "in" => FinalWithTones(Finals::IN, Tones::None),
+            "īn" => FinalWithTones(Finals::IN, Tones::One),
+            "ín" => FinalWithTones(Finals::IN, Tones::Two),
+            "ǐn" => FinalWithTones(Finals::IN, Tones::Three),
+            "ìn" => FinalWithTones(Finals::IN, Tones::Four),
+            "iou" => FinalWithTones(Finals::Iou, Tones::None),
+            "iōu" => FinalWithTones(Finals::Iou, Tones::One),
+            "ióu" => FinalWithTones(Finals::Iou, Tones::Two),
+            "iǒu" => FinalWithTones(Finals::Iou, Tones::Three),
+            "iòu" => FinalWithTones(Finals::Iou, Tones::Four),
+            "iong" => FinalWithTones(Finals::Iong, Tones::None),
+            "iōng" => FinalWithTones(Finals::Iong, Tones::One),
+            "ióng" => FinalWithTones(Finals::Iong, Tones::Two),
+            "iǒng" => FinalWithTones(Finals::Iong, Tones::Three),
+            "iòng" => FinalWithTones(Finals::Iong, Tones::Four),
+            "iu" => FinalWithTones(Finals::IU, Tones::None),
+            "iū" => FinalWithTones(Finals::IU, Tones::One),
+            "iú" => FinalWithTones(Finals::IU, Tones::Two),
+            "iǔ" => FinalWithTones(Finals::IU, Tones::Three),
+            "iù" => FinalWithTones(Finals::IU, Tones::Four),
+            "ua" => FinalWithTones(Finals::UA, Tones::None),
+            "uā" => FinalWithTones(Finals::UA, Tones::One),
+            "uá" => FinalWithTones(Finals::UA, Tones::Two),
+            "uǎ" => FinalWithTones(Finals::UA, Tones::Three),
+            "uà" => FinalWithTones(Finals::UA, Tones::Four),
+            "uo" => FinalWithTones(Finals::UO, Tones::None),
+            "uō" => FinalWithTones(Finals::UO, Tones::One),
+            "uó" => FinalWithTones(Finals::UO, Tones::Two),
+            "uǒ" => FinalWithTones(Finals::UO, Tones::Three),
+            "uò" => FinalWithTones(Finals::UO, Tones::Four),
+            "ü" => FinalWithTones(Finals::V, Tones::None),
+            "ǖ" => FinalWithTones(Finals::V, Tones::One),
+            "ǘ" => FinalWithTones(Finals::V, Tones::Two),
+            "ǚ" => FinalWithTones(Finals::V, Tones::Three),
+            "ǜ" => FinalWithTones(Finals::V, Tones::Four),
+            "üe" => FinalWithTones(Finals::VE, Tones::None),
+            "ǖe" => FinalWithTones(Finals::VE, Tones::One),
+            "ǘe" => FinalWithTones(Finals::VE, Tones::Two),
+            "ǚe" => FinalWithTones(Finals::VE, Tones::Three),
+            "ǜe" => FinalWithTones(Finals::VE, Tones::Four),
+            "üan" => FinalWithTones(Finals::Van, Tones::None),
+            "ǖan" => FinalWithTones(Finals::Van, Tones::One),
+            "ǘan" => FinalWithTones(Finals::Van, Tones::Two),
+            "ǚan" => FinalWithTones(Finals::Van, Tones::Three),
+            "ǜan" => FinalWithTones(Finals::Van, Tones::Four),
+            "ā" => FinalWithTones(Finals::A, Tones::One),
+            "á" => FinalWithTones(Finals::A, Tones::Two),
+            "ǎ" => FinalWithTones(Finals::A, Tones::Three),
+            "à" => FinalWithTones(Finals::A, Tones::Four),
+            "ō" => FinalWithTones(Finals::O, Tones::One),
+            "ó" => FinalWithTones(Finals::O, Tones::Two),
+            "ǒ" => FinalWithTones(Finals::O, Tones::Three),
+            "ò" => FinalWithTones(Finals::O, Tones::Four),
+            "ē" => FinalWithTones(Finals::E, Tones::One),
+            "é" => FinalWithTones(Finals::E, Tones::Two),
+            "ě" => FinalWithTones(Finals::E, Tones::Three),
+            "è" => FinalWithTones(Finals::E, Tones::Four),
+            "ū" => FinalWithTones(Finals::U, Tones::One),
+            "ú" => FinalWithTones(Finals::U, Tones::Two),
+            "ǔ" => FinalWithTones(Finals::U, Tones::Three),
+            "ù" => FinalWithTones(Finals::U, Tones::Four),
+            "ī" => FinalWithTones(Finals::I, Tones::One),
+            "í" => FinalWithTones(Finals::I, Tones::Two),
+            "ǐ" => FinalWithTones(Finals::I, Tones::Three),
+            "ì" => FinalWithTones(Finals::I, Tones::Four),
+            "āi" => FinalWithTones(Finals::AI, Tones::One),
+            "ái" => FinalWithTones(Finals::AI, Tones::Two),
+            "ǎi" => FinalWithTones(Finals::AI, Tones::Three),
+            "ài" => FinalWithTones(Finals::AI, Tones::Four),
+            "ēi" => FinalWithTones(Finals::EI, Tones::One),
+            "éi" => FinalWithTones(Finals::EI, Tones::Two),
+            "ěi" => FinalWithTones(Finals::EI, Tones::Three),
+            "èi" => FinalWithTones(Finals::EI, Tones::Four),
+            "ōu" => FinalWithTones(Finals::OU, Tones::One),
+            "óu" => FinalWithTones(Finals::OU, Tones::Two),
+            "ǒu" => FinalWithTones(Finals::OU, Tones::Three),
+            "òu" => FinalWithTones(Finals::OU, Tones::Four),
+            "ān" => FinalWithTones(Finals::AN, Tones::One),
+            "án" => FinalWithTones(Finals::AN, Tones::Two),
+            "ǎn" => FinalWithTones(Finals::AN, Tones::Three),
+            "àn" => FinalWithTones(Finals::AN, Tones::Four),
+            "ēn" => FinalWithTones(Finals::EN, Tones::One),
+            "én" => FinalWithTones(Finals::EN, Tones::Two),
+            "ěn" => FinalWithTones(Finals::EN, Tones::Three),
+            "èn" => FinalWithTones(Finals::EN, Tones::Four),
+            "āng" => FinalWithTones(Finals::Ang, Tones::One),
+            "áng" => FinalWithTones(Finals::Ang, Tones::Two),
+            "ǎng" => FinalWithTones(Finals::Ang, Tones::Three),
+            "àng" => FinalWithTones(Finals::Ang, Tones::Four),
+            "ēng" => FinalWithTones(Finals::Eng, Tones::One),
+            "éng" => FinalWithTones(Finals::Eng, Tones::Two),
+            "ěng" => FinalWithTones(Finals::Eng, Tones::Three),
+            "èng" => FinalWithTones(Finals::Eng, Tones::Four),
+            "ēr" => FinalWithTones(Finals::ER, Tones::One),
+            "ér" => FinalWithTones(Finals::ER, Tones::Two),
+            "ěr" => FinalWithTones(Finals::ER, Tones::Three),
+            "èr" => FinalWithTones(Finals::ER, Tones::Four),
+            _ => FinalWithTones(Finals::None, Tones::None),
+        };
+        if let FinalWithTones(Finals::None, Tones::None) = r {
+            Err(FinalWithTonesFromStrError(s.to_string()))
+        } else {
+            Ok(r)
+        }
+    }
+}
 
 impl From<FinalWithTones> for &'static str {
     fn from(value: FinalWithTones) -> Self {
         match (value.0, value.1) {
-            (_, Tones::None) => unreachable!(),
+            (_, Tones::None) => value.0.into(),
             (Finals::A, Tones::One) => "ā",
             (Finals::A, Tones::Two) => "á",
             (Finals::A, Tones::Three) => "ǎ",
@@ -228,18 +412,6 @@ impl From<FinalWithTones> for &'static str {
             (Finals::IU, Tones::Two) => "iú",
             (Finals::IU, Tones::Three) => "iǔ",
             (Finals::IU, Tones::Four) => "iù",
-            (Finals::Uan, Tones::One) => "uān",
-            (Finals::Uan, Tones::Two) => "uán",
-            (Finals::Uan, Tones::Three) => "uǎn",
-            (Finals::Uan, Tones::Four) => "uàn",
-            (Finals::Uang, Tones::One) => "uāng",
-            (Finals::Uang, Tones::Two) => "uáng",
-            (Finals::Uang, Tones::Three) => "uǎng",
-            (Finals::Uang, Tones::Four) => "uàng",
-            (Finals::Uai, Tones::One) => "uāi",
-            (Finals::Uai, Tones::Two) => "uái",
-            (Finals::Uai, Tones::Three) => "uǎi",
-            (Finals::Uai, Tones::Four) => "uài",
             (Finals::UA, Tones::One) => "uā",
             (Finals::UA, Tones::Two) => "uá",
             (Finals::UA, Tones::Three) => "uǎ",
@@ -248,10 +420,6 @@ impl From<FinalWithTones> for &'static str {
             (Finals::UO, Tones::Two) => "uó",
             (Finals::UO, Tones::Three) => "uǒ",
             (Finals::UO, Tones::Four) => "uò",
-            (Finals::Ueng, Tones::One) => "uēng",
-            (Finals::Ueng, Tones::Two) => "uéng",
-            (Finals::Ueng, Tones::Three) => "uěng",
-            (Finals::Ueng, Tones::Four) => "uèng",
             (Finals::V, Tones::One) => "ǖ",
             (Finals::V, Tones::Two) => "ǘ",
             (Finals::V, Tones::Three) => "ǚ",
@@ -264,6 +432,10 @@ impl From<FinalWithTones> for &'static str {
             (Finals::Van, Tones::Two) => "ǘan",
             (Finals::Van, Tones::Three) => "ǚan",
             (Finals::Van, Tones::Four) => "ǜan",
+            (Finals::None, Tones::One) => unreachable!(),
+            (Finals::None, Tones::Two) => unreachable!(),
+            (Finals::None, Tones::Three) => unreachable!(),
+            (Finals::None, Tones::Four) => unreachable!(),
         }
     }
 }
@@ -300,6 +472,7 @@ impl Display for PinyinDisplay {
 mod tests {
     use super::*;
     use rstest::rstest;
+    use strum::IntoEnumIterator;
 
     #[rstest]
     #[case("", Initials::None)]
@@ -357,5 +530,16 @@ mod tests {
     #[case("s", py(Initials::SH, Finals::I, Tones::One))]
     fn first_letter_format(#[case] exp: &str, #[case] val: Pinyin) {
         assert_eq!(exp, PinyinDisplay::FirstLetter(val).to_string());
+    }
+
+    #[test]
+    fn final_and_tones_to_from_str() {
+        itertools::iproduct!(Finals::iter(), Tones::iter()).for_each(|(f, t)| {
+            if f != Finals::None {
+                let s: &str = FinalWithTones(f, t).into();
+                let fwt = s.parse::<FinalWithTones>().unwrap();
+                assert_eq!(fwt, FinalWithTones(f, t));
+            }
+        });
     }
 }
